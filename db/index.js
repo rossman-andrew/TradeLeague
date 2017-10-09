3da7ca75c52df794f1df8dcaa6c18031b9579223
Andrew Rossman <andrew100r@gmail.com>
2017-10-09 09:58:26 -0700

    Updated database info

--- db/index.js
+++ db/index.js
@@ -1,6 +1,12 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection(process.env.JAWSDB_URL); 
+//const connection = mysql.createConnection(process.env.JAWSDB_URL); 
+let connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',  
+  password: '',  
+  database: 'vbfm2izfxg6l03xp'    
+});
 
 connection.connect();
 
